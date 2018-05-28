df2069899de14e845e94f2c311a2ba9e034eacba
Mick van Gelderen <mickvangelderen@gmail.com>
2018-05-28 15:44:06 +0200

    Fix test

--- glw/tests/buffer_name.rs
+++ glw/tests/buffer_name.rs
@@ -13,9 +13,9 @@ use glw::BufferNameArray;
 
 serial_test!{fn create_array() {
     let (_events_loop, _window) = support::build_display();
-    let names = <[Option<glw::BufferName>; 32]>::new();
-    for (i, n) in names.iter().enumerate() {
-        unsafe {
+    unsafe {
+        let names = <[Option<glw::BufferName>; 32]>::new();
+        for (i, n) in names.iter().enumerate() {
             assert_eq!(n.as_ref().unwrap().as_u32(), i as u32 + 1);
         }
     }
