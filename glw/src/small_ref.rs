5a9c36b74c4146efd6eff6bad359e53b3ade6710
Mick van Gelderen <mickvangelderen@gmail.com>
2018-10-22 10:42:05 +0200

    Implement Copy on SmallRef and require Freeze
    Requires `copy` to be of a type implementing `Copy`. Chose `[u8; 4]` to
    represents 4 arbitrary bytes of data.
    
    Interior mutability breaks the unsafe abstraction. `Freeze` is an
    unfortunately private auto trait from libcore. We define our own freeze
    but it needs to be understood and implemented manually by consumers of
    our library.

--- glw/src/small_ref.rs
+++ glw/src/small_ref.rs
@@ -1,50 +1,107 @@
 use core::marker::PhantomData;
-use std::mem::ManuallyDrop;
 use std::ptr;
 
 fn phantom_data_from<T>(_: T) -> PhantomData<T> {
     PhantomData
 }
 
-pub struct SmallRef<'a, T> {
-    copy: ManuallyDrop<T>,
+/// Promise that your type has no embedded/interior mutability.
+/// https://users.rust-lang.org/t/references-to-values-smaller-than-references/21448/5
+pub unsafe trait Freeze {}
+
+pub struct Ref32<'a, T: 'a>
+where
+    T: Freeze,
+{
+    copy: [u8; 4],
     _borrow: PhantomData<&'a T>,
 }
 
-impl<'a, T> SmallRef<'a, T> {
+impl<'a, T: 'a> Ref32<'a, T>
+where
+    T: Freeze,
+{
     #[inline]
     pub fn new(value: &'a T) -> Self {
         use std::mem::size_of;
+
+        assert!(
+            size_of::<T>() == size_of::<[u8; 4]>(),
+            "Ref32 can only be implemented for types that are 32 bits in size."
+        );
+
         assert!(
             size_of::<T>() < size_of::<&T>(),
-            "SmallRef only makes sense for types smaller than a pointer."
+            "Ref32 only makes sense for types smaller than a pointer."
         );
+
         unsafe {
-            SmallRef {
-                copy: ManuallyDrop::new(ptr::read(value)),
+            Ref32 {
+                copy: ptr::read(value as *const T as *const [u8; 4]),
                 _borrow: phantom_data_from(value),
             }
         }
     }
 }
 
-impl<'a, T> std::ops::Deref for SmallRef<'a, T> {
+// NOTE: Won't derive.
+impl<'a, T: 'a> Copy for Ref32<'a, T> where T: Freeze {}
+
+// NOTE: Won't derive.
+impl<'a, T: 'a> Clone for Ref32<'a, T>
+where
+    T: Freeze,
+{
+    #[inline]
+    fn clone(&self) -> Self {
+        Ref32 {
+            copy: self.copy,
+            _borrow: self._borrow,
+        }
+    }
+}
+
+impl<'a, T: 'a> std::ops::Deref for Ref32<'a, T>
+where
+    T: Freeze,
+{
     type Target = T;
 
     #[inline]
     fn deref(&self) -> &T {
-        &*self.copy
+        unsafe { &*(&self.copy as *const _ as *const T) }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::SmallRef;
+    use super::Freeze;
+    use super::Ref32;
+
+    struct Resource(u32);
+
+    impl Resource {
+        fn as_u32(&self) -> u32 {
+            self.0
+        }
+    }
+
+    unsafe impl Freeze for Resource {}
 
     #[test]
     fn it_is_indeed_smaller() {
         use std::mem::size_of;
 
-        assert!(size_of::<SmallRef<u32>>() < size_of::<&u32>());
+        assert!(size_of::<Ref32<Resource>>() < size_of::<&Resource>());
+    }
+
+    #[test]
+    fn can_copy_ref32() {
+        let x = Resource(13);
+        let r = Ref32::new(&x);
+        let r2 = r;
+        let r3 = r;
+        assert_eq!(r2.as_u32(), 13);
+        assert_eq!(r3.as_u32(), 13);
     }
 }
